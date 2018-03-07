79d589144e2f86430df3ee1efae022f40d8757bf
DreamlandSky <1739014279@qq.com>
2018-03-07 14:36:48 +0800

    learn to use junit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,4 +3,8 @@ public class main {
     public static void main(String[] args)  {
         System.out.println("Hello Gradle");
     }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
 }
---
+++ src/test/java/test.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void add(){
+        Assert.assertTrue(main.add(1,2) == 3);
+    }
+}
